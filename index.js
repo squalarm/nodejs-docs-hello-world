23f875e133ea9f26a88770e974e0f92aabda0510
MinGyu, Ju <knighp@gmail.com>
2020-02-24 09:26:49 +0900

    First Commit
    First Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello WOW!");
+    response.end("Hello Everyone!");
 });
 
 const port = process.env.PORT || 1337;
